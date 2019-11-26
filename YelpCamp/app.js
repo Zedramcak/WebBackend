4fa80237eddf051ad9f486838dc438365c8b8093
Adam Zrcek <adamzrc@gmail.com>
2019-11-26 14:19:17 +0100

    add show page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -13,48 +13,54 @@ app.set("view engine", "ejs");
 // * Schema setup
 var campGroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campGroundSchema);
 
-// ?Campground.create(
-// ? {
-// ?   name: "Mushroom Kingdom",
-// ?    image:
-// ?      "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80"
-// ?  },
-// ?  (err, campground) => {
-// ?    if (err) {
-// ?      console.log(err);
-// ?    } else {
-// ?      console.log("NEW CAMPGROUND ADDED");
-// ?      console.log(campground);
-// ?    }
-// ?  }
-// ?);
+// Campground.create(
+//   {
+//     name: "Mushroom Kingdom",
+//     image:
+//       "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80",
+//     description: "This is a huge granite hill, no bathrooms."
+//   },
+//   (err, campground) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("NEW CAMPGROUND ADDED");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
+//* show all camgrounds
 app.get("/campgrounds", (req, res) => {
   Campground.find({}, (err, campgrounds) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", { campgrounds: campgrounds });
+      res.render("index", { campgrounds: campgrounds });
     }
   });
   // *   res.render("campgrounds", { campgrounds: campgrounds });
 });
 
+//* add new campground to db
 app.post("/campgrounds", (req, res) => {
   var name = req.body.name;
   var image = req.body.image;
+  var description = req.body.description;
   var newCampground = {
     name: name,
-    image: image
+    image: image,
+    description: description
   };
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
@@ -65,8 +71,21 @@ app.post("/campgrounds", (req, res) => {
   });
 });
 
+//* create new campground
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
+});
+
+//* SHOW - shows more info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+  //* find the campground with provided id
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
 
 var port = process.env.PORT || 3000;
