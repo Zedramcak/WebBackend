42a2cb4a0b39b406cf708df0fbe8e9dc6a73256b
Adam Zrcek <adamzrc@gmail.com>
2019-11-27 13:34:28 +0100

    method-override, adding and editing blogs

--- RESTfulRouting/RESTful_BLOG_app/app.js
+++ RESTfulRouting/RESTful_BLOG_app/app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  mongoose = require("mongoose");
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
   useNewUrlParser: true,
@@ -10,6 +11,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 //* MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -36,6 +38,50 @@ app.get("/blogs", (req, res) => {
       console.log(err);
     } else {
       res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+app.get("/blogs/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/blogs", (req, res) => {
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  });
+});
+
+app.get("/blogs/:id/edit", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", { blog: foundBlog });
+    }
+  });
+});
+
+app.put("/blogs/:id", (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
     }
   });
 });
