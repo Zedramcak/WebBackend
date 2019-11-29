efce68bb6ff3af62078901e05b1040b88f5f3c88
Adam Zrcek <adamzrc@gmail.com>
2019-11-29 17:47:14 +0100

    setting user and adding requires

--- Authentication/authDemo/app.js
+++ Authentication/authDemo/app.js
@@ -1,13 +1,31 @@
-var express = require("express");
-var app = express();
-var mongoose = require("mongoose");
+const express = require("express"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  User = require("./models/user"),
+  app = express();
 
 mongoose.connect("mongodb://localhost/auth_demo_app", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+app.use(
+  require("express-session")({
+    secret: "The answer is 42",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
